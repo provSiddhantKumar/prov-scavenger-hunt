eec90c42f9a7eb391d82352e25025ae62a921830
Siddhant Kumar <siddhant.kumar@providence.org>
2023-09-22 10:01:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(path.join(__dirname, 'public', 'static')));
 
+const team1Riddle = "In a haven where wild hearts roam free,\nAmong the bleats, not the tree,\nSeek the pattern, square and quaint,\nScan its secrets, with no restraint.";
+const team2Riddle = "At beginnings and ends, pathways entwine,\nTo the side most silent, seek the subtle sign.\nAmidst the stride, a cipher lies in shade,\nCapture its essence, for the game to be played.";
+const team3Riddle = "Where journeys begin and tales intertwine,\nBetween known and unknown, there's a thin line. \nWith columns as guards, though no doorplate,\nDiscover the hint where beginnings await.";
+const team4Riddle = "Whispers of water, a ballet in play, \nWhere droplets shimmer, in a rhythmic array. \nNot the vast ocean, nor a babbling brook, \nFind the heart's desires, in this nook.";
+
 const scavengerHunts = [
     {
         teamId: 'team1',
         riddles: [
-            { id: 'aX5j9mR', passcode: 'smart1', riddleText: 'First riddle for Team Alpha...' },
-            { id: 'zQ8p7lK', passcode: 'smart2', riddleText: 'Second riddle for Team Alpha...' },
+            { id: 'aX5j9mR', passcode: 'storm', riddleText: team1Riddle },
+            // ... other riddles for Team Alpha
+        ],
+        teamId: 'team2',
+        riddles: [
+            { id: 'zQ8p7lK', passcode: 'roast', riddleText: team2Riddle },
+            // ... other riddles for Team Alpha
+        ],
+        teamId: 'team3',
+        riddles: [
+            { id: 'ajJJ9mY', passcode: 'steam', riddleText: team3Riddle },
+            // ... other riddles for Team Alpha
+        ],
+        teamId: 'team4',
+        riddles: [
+            { id: 'sfglsoe', passcode: 'clock', riddleText: team4Riddle },
             // ... other riddles for Team Alpha
         ]
     },
